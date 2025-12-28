a22d3944aa6150ff8ca35997ae3b2f9dc3032b88
Essil Balti <essilessil2@gmail.com>
2025-12-29 00:11:56 +0100

    test

--- lib/main.dart
+++ lib/main.dart
@@ -28,6 +28,7 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
+        //bygftyrtc fèfbngyè_tbn_ygf(dr-tguh_çunkjh_yh)
         colorScheme: .fromSeed(seedColor: Colors.deepPurple),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
